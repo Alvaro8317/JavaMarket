d96e4de33d90e64389cad6e124d0e5c1c64a31ea
Alvaro8317 <eduardo831_@hotmail.com>
2023-06-05 17:21:20 -0500

    feat:First commit

---
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication /*Esto indica que es la clase que gestiona la aplicación*/
+public class PlatziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarketApplication.class, args);
+	}
+
+}
